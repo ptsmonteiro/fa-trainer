e8975f3b865706fe9696b29199d015e2f58b47da
Pedro <ptsmonteiro@gmail.com>
2025-08-29 19:01:41 +0200

    Add more scenarios

--- scenarios.lua
+++ scenarios.lua
@@ -14,4 +14,43 @@ scenarios = {
             failure = pick_random({failures.sys_2_fmgc_1, failures.sys_2_fmgc_1_r})
         },
     },
+    {
+        {
+            phase = phases.takeoff,
+            failure = pick_random({
+                failures.sys_7_slat_lk_at_0,
+                failures.sys_7_slat_lk_btwn_0_1,
+                failures.sys_7_slat_lk_btwn_1_2,
+                failures.sys_7_slat_lk_btwn_3_4,
+                failures.sys_7_flap_lk_btwn_0_1,
+                failures.sys_7_flap_lk_btwn_1_2,
+                failures.sys_7_flap_lk_btwn_2_3
+            })
+        },
+    },
+    {
+        {
+            phase = phases.climb,
+            failure = pick_random({
+                failures.sys_2_fmgc_1, failures.sys_2_fmgc_1_r
+            })
+        },
+        {
+            phase = phases.climb,
+            failure = pick_random({
+                failures.sys_2_fmgc_2, failures.sys_2_fmgc_2_r
+            })
+        },
+    },
+    {
+        {
+            phase = phases.ground,
+            failure = failures.sys_6_fcdc_2_fail
+        },
+        {
+            phase = phases.climb,
+            failure = failures.sys_4_dc_bus_ess
+        },
+    },
+
 }
